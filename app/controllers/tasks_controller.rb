90204b0bca17091d5c91796f3e10bac02ce428ba
kohei sakurai <ks19ks19ks19@gmail.com>
2021-06-13 15:30:02 +0900

    add CRUD functions and create corresponding views

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,6 +8,35 @@ class TasksController < ApplicationController
   def show
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def edit
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to @task, notice: 'Task was successfully created'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to @task, notice: 'Task was successfully updated'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, notice: 'Task was successfully destroyed.'
+  end
+
   private
 
   def task_params
