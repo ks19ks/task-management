f937e69b5f6b58055f25f1758fbdccfb7e552a3f
kohei sakurai <ks19ks19ks19@gmail.com>
2021-06-13 14:40:00 +0900

    add tasks_controller and create index, show

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,20 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit update destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+end
