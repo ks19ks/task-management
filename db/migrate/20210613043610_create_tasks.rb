f2e70698bd1354ce53400c22334875c264694c83
kohei sakurai <ks19ks19ks19@gmail.com>
2021-06-13 22:09:19 +0900

    add not null constraint to title on tasks

--- db/migrate/20210613043610_create_tasks.rb
+++ db/migrate/20210613043610_create_tasks.rb
@@ -1,7 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-      t.string :title, null: false, default: ''
+      t.string :title
       t.text :description
 
       t.timestamps
