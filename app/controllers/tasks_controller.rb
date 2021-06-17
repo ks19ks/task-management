4e89e1f2ff9aa231c3de49945dcad617671f9355
kohei sakurai <ks19ks19ks19@gmail.com>
2021-06-18 08:09:45 +0900

    add handling i18n

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,7 +18,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to @task, notice: 'Task was successfully created'
+      redirect_to @task, notice: t('tasks.create_notice')
     else
       render :new
     end
@@ -26,7 +26,7 @@ class TasksController < ApplicationController
 
   def update
     if @task.update(task_params)
-      redirect_to @task, notice: 'Task was successfully updated'
+      redirect_to @task, notice: t('tasks.update_notice')
     else
       render :edit
     end
@@ -34,7 +34,7 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    redirect_to tasks_path, notice: 'Task was successfully destroyed.'
+    redirect_to tasks_path, notice: t('tasks.destroy_notice')
   end
 
   private
