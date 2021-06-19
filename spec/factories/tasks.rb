f21383b232af47b6d5a6ba77c94808d3624b77a0
kohei sakurai <ks19ks19ks19@gmail.com>
2021-06-19 09:05:09 +0900

    add context to spec to check order of tasks

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,4 +3,8 @@ FactoryBot.define do
     title { 'test_title' }
     description { 'test_description' }
   end
+  factory :task2, class: Task do
+    title { 'test_title2' }
+    description { 'test_description2' }
+  end
 end
