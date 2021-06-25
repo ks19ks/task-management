f8a7e6f36f968086ee8aa68fa3960440c6597b06
kohei sakurai <ks19ks19ks19@gmail.com>
2021-06-26 07:47:46 +0900

    add status column to tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 100 }
   validates :description, presence: true
+
+  enum status: [ '未着手', '着手中', '完了' ]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_20_134626) do
+ActiveRecord::Schema.define(version: 2021_06_23_224143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_06_20_134626) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "due_date"
+    t.integer "status"
   end
 
 end
