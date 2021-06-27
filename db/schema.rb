0ad4556a22db2e47b4bdab60e9b9b29243e7a404
kohei sakurai <ks19ks19ks19@gmail.com>
2021-06-27 12:58:58 +0900

    add index on title to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_23_224143) do
+ActiveRecord::Schema.define(version: 2021_06_27_035513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_06_23_224143) do
     t.datetime "updated_at", null: false
     t.date "due_date"
     t.integer "status"
+    t.index ["title"], name: "index_tasks_on_title"
   end
 
 end
