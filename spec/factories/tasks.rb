eeb195ce3ff7565553fb7988fdc9d6bd3fa13d43
kohei sakurai <ks19ks19ks19@gmail.com>
2021-06-27 18:21:07 +0900

    add spec to test search function

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,10 +3,18 @@ FactoryBot.define do
     title { 'test_title' }
     description { 'test_description' }
     due_date { '2021-06-01' }
+    status { '着手中' }
   end
   factory :task2, class: Task do
     title { 'test_title2' }
     description { 'test_description2' }
     due_date { '2021-07-01' }
+    status { '未着手' }
+  end
+  factory :task3, class: Task do
+    title { 'タイトルテストです' }
+    description { '説明テストです' }
+    due_date { '2021-08-01' }
+    status { '完了' }
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -3,18 +3,50 @@ RSpec.describe 'タスク管理機能', type: :system do
   before do
     FactoryBot.create(:task)
     FactoryBot.create(:task2)
+    FactoryBot.create(:task3)
   end
+
+  describe "検索機能" do
+    before do
+      visit tasks_path
+    end
+    context "タイトルであいまい検索した場合" do
+      it "検索キーワードを含むタスクで絞り込まれる" do
+        fill_in "タイトル",	with: "タイトル"
+        click_on '検索'
+        expect(page).to have_content 'タイトルテスト'
+      end
+    end
+    context "ステータス検索した場合" do
+      it 'ステータスに完全一致するタスクに絞り込まれる' do
+        select '完了', from: 'ステータス'
+        click_on '検索'
+        expect(page).to have_content 'タイトルテスト'
+      end
+    end
+    context 'タイトルのあいまい検索とステータス検索をした場合' do
+      it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
+        fill_in "タイトル",	with: "タイトル"
+        select '完了', from: 'ステータス'
+        click_on '検索'
+        expect(page).to have_content 'タイトルテスト'
+      end
+    end
+  end
+
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
         visit new_task_path
         fill_in "タイトル",	with: "new task"
         fill_in "説明",	with: "task content"
+        select '完了', from: 'ステータス'
         click_on '登録する'
-        expect(page).to have_content 'new task'
+        expect(page).to have_content '完了'
       end
     end
   end
+
   describe '一覧表示機能' do
     let!(:task) { FactoryBot.create(:task, title: 'new task', due_date: '2021-05-01') }
     before do
@@ -45,6 +77,7 @@ RSpec.describe 'タスク管理機能', type: :system do
       end
     end
   end
+
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
