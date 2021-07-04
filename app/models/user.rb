694227d10b465f0894431e546e49e8dba5b5a4e6
kohei sakurai <ks19ks19ks19@gmail.com>
2021-07-04 17:06:37 +0900

    add user model and controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
   has_secure_password
 
   has_many :tasks
