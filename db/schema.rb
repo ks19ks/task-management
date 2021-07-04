e477a3747468031d7c8b8376e175451cd072ccba
kohei sakurai <ks19ks19ks19@gmail.com>
2021-07-04 11:55:07 +0900

    add user model, association with task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_27_092350) do
+ActiveRecord::Schema.define(version: 2021_07_04_003410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,19 @@ ActiveRecord::Schema.define(version: 2021_06_27_092350) do
     t.date "due_date"
     t.integer "status"
     t.integer "priority"
+    t.bigint "user_id"
     t.index ["title"], name: "index_tasks_on_title"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  add_foreign_key "tasks", "users"
 end
