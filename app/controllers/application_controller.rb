42e6040de18d65f2fb4581db17470ddaf9f44fc3
kohei sakurai <ks19ks19ks19@gmail.com>
2021-07-06 09:03:19 +0900

    add login_required

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :basic_auth
+  include SessionsHelper
+  before_action :login_required
+  # before_action :basic_auth
 
   private
 
@@ -8,5 +10,9 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
+  end
+
+  def login_required
+    redirect_to login_path unless current_user
   end
 end
