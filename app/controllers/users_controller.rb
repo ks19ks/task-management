f318d30bc1d1068c67af3971f8fe1f231b5fa5a1
kohei sakurai <ks19ks19ks19@gmail.com>
2021-07-08 08:04:03 +0900

    get users login once user is created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,17 @@ class UsersController < ApplicationController
   skip_before_action :login_required, only: %i(new create)
 
   def new
-    @user = User.new
+    if logged_in?
+      redirect_to user_path(current_user)
+    else
+      @user = User.new
+    end
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
       render :new
