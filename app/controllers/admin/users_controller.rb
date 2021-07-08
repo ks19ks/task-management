17d989f6ecd42c4ef94030246c6d5fbc2a3e8ccc
kohei sakurai <ks19ks19ks19@gmail.com>
2021-07-09 08:51:39 +0900

    add admin/users controller and views

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,23 @@
+class Admin::UsersController < ApplicationController
+  def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks
   resources :users, only: %i(new create show)
+
+  namespace :admin do
+    resources :users
+  end
 end
