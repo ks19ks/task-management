49ad81247a28735c16d29a07d11003ddeeacadfa
kohei sakurai <ks19ks19ks19@gmail.com>
2021-07-11 16:46:23 +0900

    add callback not to delete last one admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,21 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  before_destroy :cannot_destroy_last_admin
+  before_update :cannot_update_last_admin
+
   has_many :tasks, dependent: :destroy
+
+  private
+
+  def cannot_destroy_last_admin
+    throw(:abort) if User.where(admin: true).count == 1 && self.admin?
+  end
+
+  def cannot_update_last_admin
+    if User.where(admin: true).count == 1 && User.find_by(admin: true).id == self.id && self.admin == false
+      errors.add(:base, '管理者権限を外すことができません')
+      throw(:abort)
+    end
+  end
 end
